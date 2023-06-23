81f8f50eb7f09f77acdf286bfb6e829b0b1bda42
Keith Tachibana <Keith.Tachibana@gmail.com>
2023-06-23 16:58:49 -0700

    Fixing media queries

--- app/components/MenuBar.jsx
+++ app/components/MenuBar.jsx
@@ -14,19 +14,20 @@ import {
 } from 'semantic-ui-react';
 
 export default class MenuBar extends Component {
-  state = {};
+  state = {clicked: true};
 
+  handleMenuClick = () => this.setState({ clicked: !this.state.clicked });
   handleItemClick = (event, { name }) => this.setState({ activeItem: name });
   toggleFixedMenu = inView => this.setState({ fixed: !inView });
 
   render() {
     const { activeItem, fixed } = this.state;
 
-    const [menuOpen, setMenuOpen] = useState(false);
-
     const segmentStyle = {
       backgroundColor: '#000000'
     };
+
+    const menuStyle = this.state.clicked ? 'menu-open' : 'menu-closed';
 
     return (
       <InView onChange={this.toggleFixedMenu}>
@@ -47,70 +48,72 @@ export default class MenuBar extends Component {
               style={segmentStyle}
               className="menu-wrapper"
             >
-              <Container text className="menu-ul">
-                <Image
-                  as="a"
-                  href="/"
-                  size="small"
-                  alt="Alt logo"
-                  src="/assets/Logos/Logos_Full_Name/Best_Logo_Wide_Name_No_Background.png"
-                />
-                <Menu.Item
-                  as="a"
-                  href="/"
-                  name="home"
-                  active={activeItem === "home"}
-                  onClick={this.handleItemClick}
-                >
-                  Home
-                </Menu.Item>
-                <Menu.Item
-                  as="a"
-                  href="/services"
-                  name="services"
-                  active={activeItem === "services"}
-                  onClick={this.handleItemClick}
-                >
-                  Services
-                </Menu.Item>
-                <Menu.Item
-                  as="a"
-                  href="/about"
-                  name="about"
-                  active={activeItem === "about"}
-                  onClick={this.handleItemClick}
-                >
-                  About
-                </Menu.Item>
-                <Menu.Item
-                  as="a"
-                  href="/contact"
-                  name="contact"
-                  active={activeItem === "contact"}
-                  onClick={this.handleItemClick}
-                >
-                  Contact Us
-                </Menu.Item>
-                <Menu.Item position='right' className='menu-nologo'>
-                  <Button as="a" href="/demo" inverted color="teal">
-                    Free Consultation
-                  </Button>
-                </Menu.Item>
-                <button className="hamburger">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="white"
+              <nav className={menuStyle}>
+                <Container text className="menu-ul">
+                  <Image
+                    as="a"
+                    href="/"
+                    size="small"
+                    alt="Alt logo"
+                    src="/assets/Logos/Logos_Full_Name/Best_Logo_Wide_Name_No_Background.png"
+                  />
+                  <Menu.Item
+                    as="a"
+                    href="/"
+                    name="home"
+                    active={activeItem === "home"}
+                    onClick={this.handleItemClick}
                   >
-                    <path
-                      fillRule="evenodd"
-                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
-              </Container>
+                    Home
+                  </Menu.Item>
+                  <Menu.Item
+                    as="a"
+                    href="/services"
+                    name="services"
+                    active={activeItem === "services"}
+                    onClick={this.handleItemClick}
+                  >
+                    Services
+                  </Menu.Item>
+                  <Menu.Item
+                    as="a"
+                    href="/about"
+                    name="about"
+                    active={activeItem === "about"}
+                    onClick={this.handleItemClick}
+                  >
+                    About
+                  </Menu.Item>
+                  <Menu.Item
+                    as="a"
+                    href="/contact"
+                    name="contact"
+                    active={activeItem === "contact"}
+                    onClick={this.handleItemClick}
+                  >
+                    Contact Us
+                  </Menu.Item>
+                  <Menu.Item position='right'>
+                    <Button as="a" href="/demo" inverted color="teal">
+                      Free Consultation
+                    </Button>
+                  </Menu.Item>
+                  <button className="hamburger" onClick={this.handleMenuClick}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="white"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </Container>
+              </nav>
             </Menu>
           </Suspense>
         </Segment>
