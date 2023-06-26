5f4f4f905bfe98dd02865697b8e6ecc858cbac28
Keith Tachibana <Keith.Tachibana@gmail.com>
2023-06-25 18:08:38 -0700

    Fixed routes

--- app/api/route.js
+++ app/api/route.js
@@ -10,14 +10,3 @@ export async function GET() {
 
   return NextResponse.json({ data });
 };
-
-export async function GET() {
-  const res = await fetch('/api/route/demo', {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-  const data = await res.json();
-
-  return NextResponse.json({ data });
-};
