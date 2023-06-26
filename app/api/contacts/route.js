ae81f68eba675b30fc22d24433064dc37ddc02c2
Keith Tachibana <Keith.Tachibana@gmail.com>
2023-06-25 18:12:17 -0700

    More route issues

--- app/api/contacts/route.js
+++ app/api/contacts/route.js
@@ -1,1 +1,12 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+  const res = await fetch("/api/route/contacts", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await res.json();
+
+  return NextResponse.json({ data });
+}
--- app/api/demo/route.js
+++ app/api/demo/route.js
@@ -1,1 +1,12 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+  const res = await fetch("/api/route/demo", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await res.json();
+
+  return NextResponse.json({ data });
+}
